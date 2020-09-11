bbf35d8e3661061aa43aed830b8247913d305def
Андрей Беляков <ttxiom@gmail.com>
2020-09-11 18:32:16 +0500

    new

---
+++ tasks/PythonBegin/01.py
@@ -0,0 +1,17 @@
+
+enum = range(5, 10, 2)
+
+print(enum)
+
+for item in enum:
+    print(item)
+
+print(enum[0])
+
+# lst = [1, 2, 3]
+# for i in range(len(lst)):
+#     print(lst[i])
+
+# lst = [1, 2, 3]
+# for item in lst:
+#     print(item)
